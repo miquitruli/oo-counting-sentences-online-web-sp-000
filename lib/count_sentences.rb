40e8a8e6678bb991fe3a49df394c5ce7a300e1fb
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-13 23:14:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    sentences=self.split(/!?./)
+    sentences = self.split(/[!?.]/)
     sentences.delete_if{|sentence| sentence.empty?}
     sentences.length
   end
