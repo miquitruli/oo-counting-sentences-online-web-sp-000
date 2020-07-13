8bcb80a9dde9a9cf6507987dad3d4a3cb306d0ba
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-13 23:33:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/[!?.]/)
+    sentences.delete_if{|sentence| sentence.empty?}
+    sentences.length
   end
 end
